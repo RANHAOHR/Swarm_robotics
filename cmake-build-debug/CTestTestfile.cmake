429df17e29402d126352da2137c46fdc73924e07
RANHAOHR <rxh349@case.edu>
2017-09-29 15:58:00 -0500

    add robot state publisher

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ranhao/ros_ws/src/swarm_robotics
+# Build directory: /home/ranhao/ros_ws/src/swarm_robotics/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
