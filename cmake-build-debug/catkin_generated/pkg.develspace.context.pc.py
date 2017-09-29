a55f8704da20aa9a3962dc02672bc255fa44c845
RANHAOHR <rxh349@case.edu>
2017-09-29 15:24:01 -0500

    add three robots and test the control params

--- cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/ranhao/ros_ws/src/swarm_robotics/include".split(';') if "/home/ranhao/ros_ws/src/swarm_robotics/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "swarm_robotics"
-PROJECT_SPACE_DIR = "/home/ranhao/ros_ws/src/swarm_robotics/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
