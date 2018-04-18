ff056a1e7f7d145a571baf1377913eb446e4cde1
RANHAOHR <rxh349@case.edu>
2018-04-18 17:11:05 -0500

    add controller class, waiting for stateflow model to build up

--- cmake-build-debug/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs;geometry_msgs;nav_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lmulti_agent_controller".split(';') if "-lmulti_agent_controller" != "" else []
-PROJECT_NAME = "multiAgent_system_modeling"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
